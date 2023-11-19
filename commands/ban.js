428ba63b8b04865bdf2de713978bd9f2420426ef
BNT Feujjj <148008611+BNTFeujjj@users.noreply.github.com>
2023-11-19 16:27:46 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,21 +1,31 @@
-const db = require('quick.db')
-const Discord = require('discord.js')
+const db = require('quick.db');
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'ban',
     aliases: [],
     run: async (client, message, args, prefix) => {
-        if (message.member.permissions.has("BAN_MEMBERS")){
-            let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-            if (!member) member = await message.guild.members.fetch(args[0]).catch(() => {return message.reply(`❌ Aucune personne trouvée !`)})
+        if (message.member.permissions.has("BAN_MEMBERS")) {
+            let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+            if (!member) member = await message.guild.members.fetch(args[0]).catch(() => { return message.reply(`❌ Aucune personne trouvée !`); });
 
-            if (member){
-                if (member.user.id === message.author.id) return message.reply("❌ Vous ne pouvez pas vous bannir vous-même !")
-                let raison = args.slice(1).join(' ')
-                if (!raison) raison = "Aucune raison donnée"
-                await member.ban(`Banni par ${message.author.tag} pour ${raison}`).then(() => {
-                    message.reply(`✅ ${member.user.username}#${member.user.tag} a été banni par ${message.author.tag} pour la raison ${raison}`)
-                })
+            if (member) {
+                if (member && member.user && member.user.id === message.author.id) {
+                    return message.reply("❌ Vous ne pouvez pas vous bannir vous-même !");
+                }
+                
+
+                let raison = args.slice(1).join(' ') || "Aucune raison";
+                if (!raison) raison = "Aucune raison donnée";
+
+                // Use guild.members.ban() instead of member.ban()
+                await message.guild.members.ban(member, { reason: `Banni par ${message.author.username} pour la raison : ${raison}` }).then(() => {
+                    message.reply(`✅ ${member.user.username}#${member.user.tag} a été banni par ${message.author.tag} pour la raison : ${raison}`);
+                }).catch(error => {
+                    message.reply('❌ Une erreur s\'est produite lors du bannissement de ce membre.');
+                    console.error(error);
+                });
             }
         }
     }
-}
+};
