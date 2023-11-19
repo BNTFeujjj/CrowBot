c5a1f7db8eaabbc8aef587f27893a142fcf597d4
BNT Feujjj <148008611+BNTFeujjj@users.noreply.github.com>
2023-11-19 16:27:24 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,15 +1,31 @@
-const ms = require('ms')
+const ms = require('ms');
 
 module.exports = {
     name: 'unban',
     aliases: [],
     run: async (client, message, args, prefix, color) => {
-        if (message.member.permissions.has("BAN_MEMBERS")){
-            let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-        if (!member) return message.reply(`❌ Aucune personne trouvée !`)
+        if (message.member.permissions.has("BAN_MEMBERS")) {
+            let userId = args[0];
 
-        
-        member.unban(`Unbanni par ${message.author.tag} pour ${raison}`).then(() => message.reply(`<@${member.user.id}> a été débanni`).catch(() => false)).catch(() => message.reply("Impossible de debannir ce membre").catch(() => false))
+            if (!userId) {
+                return message.reply("❌ Veuillez spécifier l'ID de l'utilisateur à débannir.");
+            }
+
+            if (isNaN(userId) || !Number.isInteger(Number(userId))) {
+                return message.reply("❌ L'ID doit être un nombre valide.");
+            }
+
+            try {
+
+                await message.guild.members.unban(userId, `Unbanni par ${message.author.tag}`);
+                message.reply(`✅ Utilisateur avec l'ID ${userId} a été débanni.`);
+            } catch (error) {
+               
+                console.error(error);
+                message.reply("❌ Une erreur s'est produite lors du débannissement de cet utilisateur.");
+            }
+        } else {
+            message.reply("❌ Vous n'avez pas la permission de débannir des membres.");
         }
     }
-}
+};
