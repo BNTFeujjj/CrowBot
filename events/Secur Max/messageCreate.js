a7a6c462dd2958a6d1321e626dbbeedd62f765a1
BNT Feujjj <148008611+BNTFeujjj@users.noreply.github.com>
2023-11-19 14:37:26 +0100

    Update messageCreate.js

--- events/Secur Max/messageCreate.js
+++ events/Secur Max/messageCreate.js
@@ -7,15 +7,16 @@ module.exports = async (client, message) => {
     let logs = guild.channels.cache.get(db.get(`logs_${guild.id}`))
 
 if (db.get(`link_${message.guild.id}`) === true){
-if (message.content.includes("discord.gg" || "discord.com/invite" || "discordapp.com/invite")){
+if (message.content.match(/(discord\.gg\/([a-zA-Z0-9]+)|discordapp\.com\/invite\/([a-zA-Z0-9]+))|(https:\/\/([a-zA-Z0-9]+)|http:\/\/([a-zA-Z0-9]+))/)){
     if (message.member.permissions.has("ADMINISTRATOR")) return;
     if (message.author.id === client.user.id) return;
-message.delete().catch(() => false)
 let protected = new MessageEmbed()
 .setTitle("**LOGS**")
-.setDescription(`L'anti-invite vien d'être detectée une personne a envoyé une invitation non autorisée.
-La personne est <@${action.executor.id}>`)
+.setDescription(`L'anti-invite vien d'être utilisé. Une personne a envoyé une invitation ou un lien non autorisée.\n
+La personne est <@${message.author.id}>`)
 .setColor("RED")
+message.delete().catch(() => false)
+
 if (logs) logs.send({embeds: [protected]}).catch(() => false)
 }
 }
